25a770d05a75ca039ea0bae41b4de76d050007c5
steve-ng562 <sjng1@uci.edu>
2019-01-04 00:02:27 -0800

    Updates

---
+++ precache-manifest.7ce8d4352ffe8297bed67e35207a1fd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f0a4012e0d7708998b9",
+    "url": "/digipops-coding-test/static/js/runtime~main.8f0a4012.js"
+  },
+  {
+    "revision": "ad8f56133f3a7c3d00d2",
+    "url": "/digipops-coding-test/static/js/main.ad8f5613.chunk.js"
+  },
+  {
+    "revision": "8f96860354c388943ff0",
+    "url": "/digipops-coding-test/static/js/1.8f968603.chunk.js"
+  },
+  {
+    "revision": "ad8f56133f3a7c3d00d2",
+    "url": "/digipops-coding-test/static/css/main.9c434d38.chunk.css"
+  },
+  {
+    "revision": "8f96860354c388943ff0",
+    "url": "/digipops-coding-test/static/css/1.0925ab31.chunk.css"
+  },
+  {
+    "revision": "2a0a44877168634b935243203d67bef4",
+    "url": "/digipops-coding-test/index.html"
+  }
+];
